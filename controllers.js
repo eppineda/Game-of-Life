7a2b9467b5cadde593e5e0c300dfee39c6032546
Enrique Pineda <enrique.pineda@gmail.com>
2014-12-19 10:16:35 -0500

    dynamic table row creation for the cell coords

--- controllers.js
+++ controllers.js
@@ -5,6 +5,7 @@
 
 angular.module('gameoflife.controllers', [])
 .controller('GridController', function($scope) {
+    $scope.grid = { 'width':3, 'height':3 }
     $scope.resize = function() {
         if ('undefined' == $scope.grid.width || 'undefined' == $scope.grid.height || 3 >
             $scope.grid.width || 3 > $scope.grid.height)
@@ -13,8 +14,15 @@ angular.module('gameoflife.controllers', [])
     }
 })
 .controller('SeedController', function($scope) {
+    $scope.seed = { 'howMany':0, 'cells':[] }
     $scope.setRows = function() {
-        console.log($scope.seed.howMany)
+        if ('undefined' == $scope.seed.howMany || 1 > $scope.seed.howMany) return
+        for (var i = 0, j = $scope.seed.howMany; i < j; ++i)
+            $scope.seed.cells[i] = { x:-1, y:-1 } // temporary. will become a cell later.
+        console.log($scope.seed.cells.length)
+    }
+    $scope.setCoordinate = function(index) {
+        console.log($scope.seed.cells)
     }
     $scope.createCells = function() {
         console.log('randomly creating cells')
