dbcd4f7a3d74a71b4129a91e567f716c2178e8a1
Enrique Pineda <enrique.pineda@gmail.com>
2014-12-31 01:51:04 -0500

    just in case there is a name collision

--- services.js
+++ services.js
@@ -25,16 +25,16 @@ angular.module('gameoflife.services', [])
             // apply each rule to each cell
 
             var cells = currentGen
-            var nextGeneration = []
+            var nextGen = []
             for (var a = 0, b = cells.length; a < b; ++a) {
                 var afterTransition = null
 
                 for (var x = 0, y = transitions.length; x < y; ++x) {
                     afterTransition = transitions[x](cells[a])
                 }
-                nextGeneration.push(afterTransition)
+                nextGen.push(afterTransition)
             }
-            return nextGeneration
+            return nextGen
         } // nextGeneration
     } // return
 })
